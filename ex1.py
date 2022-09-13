c0927010ac06d7e3768b54bc9b7f8868eb94546e
Hieu Nguyen <hieu8hieu8@gmail.com>
2022-09-13 10:48:32 +0300

    ex1

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# print("hihi")
+# print(5*6)
+
+# Python is a high level programming language.
+# Python is an object-oriented language.
+# Python is an interpreted language.
+
+MY_VARIABLE="Hello"
+# # MY_VARIABLE=55
+print(MY_VARIABLE)
